a7def221f89aa07f6510d75e8f60bda20943debd
Fabio Fehr <fabio.fehr@idiap.ch>
2024-04-17 16:16:34 +0200

    intial commit from hushell/pmf_cvpr22

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
-import sys
 import datetime
+import json
 import random
-import numpy as np
+import sys
 import time
+from pathlib import Path
+
+import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-import json
-
-from pathlib import Path
-from torch.utils.tensorboard import SummaryWriter
-
 from timm.data import Mixup
 from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
-from timm.scheduler import create_scheduler
 from timm.optim import create_optimizer
-from timm.utils import NativeScaler, get_state_dict, ModelEma
+from timm.scheduler import create_scheduler
+from timm.utils import ModelEma, NativeScaler, get_state_dict
+from torch.utils.tensorboard import SummaryWriter
 
-from engine import train_one_epoch, evaluate
 import utils.deit_util as utils
 from datasets import get_loaders
-from utils.args import get_args_parser
+from engine import evaluate, train_one_epoch
 from models import get_model
+from utils.args import get_args_parser
 
 
 def main(args):
     utils.init_distributed_mode(args)
 
     print(args)
-    device = torch.device(args.device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # fix the seed for reproducibility
     seed = args.seed + utils.get_rank()
@@ -53,12 +52,18 @@ def main(args):
     ##############################################
     # Mixup regularization (by default OFF)
     mixup_fn = None
-    mixup_active = args.mixup > 0 or args.cutmix > 0. or args.cutmix_minmax is not None
+    mixup_active = args.mixup > 0 or args.cutmix > 0.0 or args.cutmix_minmax is not None
     if mixup_active:
         mixup_fn = Mixup(
-            mixup_alpha=args.mixup, cutmix_alpha=args.cutmix, cutmix_minmax=args.cutmix_minmax,
-            prob=args.mixup_prob, switch_prob=args.mixup_switch_prob, mode=args.mixup_mode,
-            label_smoothing=args.smoothing, num_classes=args.nClsEpisode)
+            mixup_alpha=args.mixup,
+            cutmix_alpha=args.cutmix,
+            cutmix_minmax=args.cutmix_minmax,
+            prob=args.mixup_prob,
+            switch_prob=args.mixup_switch_prob,
+            mode=args.mixup_mode,
+            label_smoothing=args.smoothing,
+            num_classes=args.nClsEpisode,
+        )
 
     ##############################################
     # Model
@@ -67,43 +72,45 @@ def main(args):
     model = get_model(args)
     model.to(device)
 
-    model_ema = None # (by default OFF)
+    model_ema = None  # (by default OFF)
     if args.model_ema:
         # Important to create EMA model after cuda(), DP wrapper, and AMP but before SyncBN and DDP wrapper
         model_ema = ModelEma(
             model,
             decay=args.model_ema_decay,
-            device='cpu' if args.model_ema_force_cpu else '',
-            resume='')
+            device="cpu" if args.model_ema_force_cpu else "",
+            resume="",
+        )
 
     model_without_ddp = model
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu],
-                                                          find_unused_parameters=args.unused_params)
+        model = torch.nn.parallel.DistributedDataParallel(
+            model, device_ids=[args.gpu], find_unused_parameters=args.unused_params
+        )
         model_without_ddp = model.module
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print('number of params:', n_parameters)
+    print("number of params:", n_parameters)
 
     ##############################################
     # Optimizer & scheduler & criterion
     if args.fp16:
-        scale = 1 / 8 # the default lr is for 8 GPUs
+        scale = 1 / 8  # the default lr is for 8 GPUs
         linear_scaled_lr = args.lr * utils.get_world_size() * scale
         args.lr = linear_scaled_lr
 
     loss_scaler = NativeScaler() if args.fp16 else None
 
-    #optimizer = create_optimizer(args, model_without_ddp)
+    # optimizer = create_optimizer(args, model_without_ddp)
     optimizer = torch.optim.SGD(
         [p for p in model_without_ddp.parameters() if p.requires_grad],
         args.lr,
         momentum=args.momentum,
-        weight_decay=0, # no weight decay for fine-tuning
+        weight_decay=0,  # no weight decay for fine-tuning
     )
 
     lr_scheduler, _ = create_scheduler(args, optimizer)
 
-    if args.mixup > 0.:
+    if args.mixup > 0.0:
         # smoothing is handled with mixup label transform
         criterion = SoftTargetCrossEntropy()
     elif args.smoothing:
@@ -114,31 +121,37 @@ def main(args):
     ##############################################
     # Resume training from ckpt (model, optimizer, lr_scheduler, epoch, model_ema, scaler)
     if args.resume:
-        if args.resume.startswith('https'):
+        if args.resume.startswith("https"):
             checkpoint = torch.hub.load_state_dict_from_url(
-                args.resume, map_location='cpu', check_hash=True)
+                args.resume, map_location="cpu", check_hash=True
+            )
         else:
-            checkpoint = torch.load(args.resume, map_location='cpu')
+            checkpoint = torch.load(args.resume, map_location="cpu")
 
-        model_without_ddp.load_state_dict(checkpoint['model'])
+        model_without_ddp.load_state_dict(checkpoint["model"])
 
-        if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
-            args.start_epoch = checkpoint['epoch'] + 1
+        if (
+            not args.eval
+            and "optimizer" in checkpoint
+            and "lr_scheduler" in checkpoint
+            and "epoch" in checkpoint
+        ):
+            optimizer.load_state_dict(checkpoint["optimizer"])
+            lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+            args.start_epoch = checkpoint["epoch"] + 1
             if args.model_ema:
-                utils._load_checkpoint_for_ema(model_ema, checkpoint['model_ema'])
-            if 'scaler' in checkpoint:
-                loss_scaler.load_state_dict(checkpoint['scaler'])
+                utils._load_checkpoint_for_ema(model_ema, checkpoint["model_ema"])
+            if "scaler" in checkpoint:
+                loss_scaler.load_state_dict(checkpoint["scaler"])
 
-        print(f'Resume from {args.resume} at epoch {args.start_epoch}.')
+        print(f"Resume from {args.resume} at epoch {args.start_epoch}.")
 
     ##############################################
     # Test
-    test_stats = evaluate(data_loader_val, model, criterion, device, args.seed+10000)
+    test_stats = evaluate(data_loader_val, model, criterion, device, args.seed + 10000)
     print(f"Accuracy of the network on dataset_val: {test_stats['acc1']:.1f}%")
     if args.output_dir and utils.is_main_process():
-        test_stats['epoch'] = -1
+        test_stats["epoch"] = -1
         with (output_dir / "log.txt").open("a") as f:
             f.write(json.dumps(test_stats) + "\n")
 
@@ -154,63 +167,76 @@ def main(args):
 
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
-    max_accuracy = test_stats['acc1']
+    # max_accuracy = test_stats["acc1"]
 
     for epoch in range(args.start_epoch, args.epochs):
 
         train_stats = train_one_epoch(
-            data_loader_train, model, criterion, optimizer, epoch, device,
-            loss_scaler, args.fp16, args.clip_grad, model_ema, mixup_fn, writer,
-            set_training_mode=False  # TODO: may need eval mode for finetuning
+            data_loader_train,
+            model,
+            criterion,
+            optimizer,
+            epoch,
+            device,
+            loss_scaler,
+            args.fp16,
+            args.clip_grad,
+            model_ema,
+            mixup_fn,
+            writer,
+            set_training_mode=False,  # TODO: may need eval mode for finetuning
         )
 
         lr_scheduler.step(epoch)
 
-        test_stats = evaluate(data_loader_val, model, criterion, device, args.seed+10000)
+        test_stats = evaluate(data_loader_val, model, criterion, device, args.seed + 10000)
 
-        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
-                     **{f'test_{k}': v for k, v in test_stats.items()},
-                     'epoch': epoch,
-                     'n_parameters': n_parameters}
+        log_stats = {
+            **{f"train_{k}": v for k, v in train_stats.items()},
+            **{f"test_{k}": v for k, v in test_stats.items()},
+            "epoch": epoch,
+            "n_parameters": n_parameters,
+        }
 
         if args.output_dir:
-            checkpoint_paths = [output_dir / 'checkpoint.pth', output_dir / 'best.pth']
+            checkpoint_paths = [output_dir / "checkpoint.pth", output_dir / "best.pth"]
             for checkpoint_path in checkpoint_paths:
                 state_dict = {
-                    'model': model_without_ddp.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'lr_scheduler': lr_scheduler.state_dict(),
-                    'epoch': epoch,
-                    'model_ema': get_state_dict(model_ema) if args.model_ema else None,
-                    'args': args,
+                    "model": model_without_ddp.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "lr_scheduler": lr_scheduler.state_dict(),
+                    "epoch": epoch,
+                    "model_ema": get_state_dict(model_ema) if args.model_ema else None,
+                    "args": args,
                 }
                 if loss_scaler is not None:
-                    state_dict['scalar'] = loss_scaler.state_dict()
+                    state_dict["scalar"] = loss_scaler.state_dict()
                 utils.save_on_master(state_dict, checkpoint_path)
 
                 if test_stats["acc1"] <= max_accuracy:
-                    break # do not save best.pth
+                    break  # do not save best.pth
 
         print(f"Accuracy of the network on dataset_val: {test_stats['acc1']:.1f}%")
         max_accuracy = max(max_accuracy, test_stats["acc1"])
-        print(f'Max accuracy: {max_accuracy:.2f}%')
+        print(f"Max accuracy: {max_accuracy:.2f}%")
 
         if args.output_dir and utils.is_main_process():
-            log_stats['best_test_acc'] = max_accuracy
+            log_stats["best_test_acc"] = max_accuracy
             with (output_dir / "log.txt").open("a") as f:
                 f.write(json.dumps(log_stats) + "\n")
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print('Training time {}'.format(total_time_str))
+    print("Training time {}".format(total_time_str))
 
     if utils.is_main_process():
         writer.close()
         import tables
+
         tables.file._open_files.close_all()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = get_args_parser()
     args = parser.parse_args()
 
--- models/vit_google.py
+++ models/vit_google.py
@@ -1,23 +1,20 @@
 import copy
 import logging
 import math
-
 from os.path import join as pjoin
 
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
-
-from torch.nn import Dropout, Softmax, Linear, Conv2d, LayerNorm
-from torch.nn.modules.utils import _pair
 from scipy import ndimage
+from torch.nn import Conv2d, Dropout, LayerNorm, Linear, Softmax
+from torch.nn.modules.utils import _pair
 
-from .utils import get_b16_config
 from .resnet_v2 import ResNetV2
-
+from .utils import get_b16_config
 
 CONFIGS = {
-    'ViT-B_16': get_b16_config(),
+    "ViT-B_16": get_b16_config(),
     #'ViT-B_32': get_b32_config(),
     #'ViT-L_16': get_l16_config(),
     #'ViT-L_32': get_l32_config(),
@@ -123,8 +120,8 @@ class Mlp(nn.Module):
 
 
 class Embeddings(nn.Module):
-    """Construct the embeddings from patch, position embeddings.
-    """
+    """Construct the embeddings from patch, position embeddings."""
+
     def __init__(self, config, img_size, in_channels=3):
         super(Embeddings, self).__init__()
         self.hybrid = None
@@ -141,15 +138,18 @@ class Embeddings(nn.Module):
             self.hybrid = False
 
         if self.hybrid:
-            self.hybrid_model = ResNetV2(block_units=config.resnet.num_layers,
-                                         width_factor=config.resnet.width_factor)
+            self.hybrid_model = ResNetV2(
+                block_units=config.resnet.num_layers, width_factor=config.resnet.width_factor
+            )
             in_channels = self.hybrid_model.width * 16
         self.patch_size = patch_size
-        self.patch_embeddings = Conv2d(in_channels=in_channels,
-                                       out_channels=config.hidden_size,
-                                       kernel_size=patch_size,
-                                       stride=patch_size)
-        self.position_embeddings = nn.Parameter(torch.zeros(1, n_patches+1, config.hidden_size))
+        self.patch_embeddings = Conv2d(
+            in_channels=in_channels,
+            out_channels=config.hidden_size,
+            kernel_size=patch_size,
+            stride=patch_size,
+        )
+        self.position_embeddings = nn.Parameter(torch.zeros(1, n_patches + 1, config.hidden_size))
         self.cls_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
 
         self.dropout = Dropout(config.transformer["dropout_rate"])
@@ -168,11 +168,13 @@ class Embeddings(nn.Module):
         # see discussion at https://github.com/facebookresearch/dino/issues/8
         w0, h0 = w0 + 0.1, h0 + 0.1
         patch_pos_embed = nn.functional.interpolate(
-            patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(0, 3, 1, 2),
+            patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(
+                0, 3, 1, 2
+            ),
             scale_factor=(h0 / math.sqrt(N), w0 / math.sqrt(N)),
-            mode='bicubic',
+            mode="bicubic",
             align_corners=False,
-            recompute_scale_factor=False
+            recompute_scale_factor=False,
         )
         assert int(h0) == patch_pos_embed.shape[-2] and int(w0) == patch_pos_embed.shape[-1]
         patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
@@ -223,10 +225,26 @@ class Block(nn.Module):
     def load_from(self, weights, n_block):
         ROOT = f"Transformer/encoderblock_{n_block}"
         with torch.no_grad():
-            query_weight = np2th(weights[pjoin(ROOT, ATTENTION_Q, "kernel")]).view(self.hidden_size, self.hidden_size).t()
-            key_weight = np2th(weights[pjoin(ROOT, ATTENTION_K, "kernel")]).view(self.hidden_size, self.hidden_size).t()
-            value_weight = np2th(weights[pjoin(ROOT, ATTENTION_V, "kernel")]).view(self.hidden_size, self.hidden_size).t()
-            out_weight = np2th(weights[pjoin(ROOT, ATTENTION_OUT, "kernel")]).view(self.hidden_size, self.hidden_size).t()
+            query_weight = (
+                np2th(weights[pjoin(ROOT, ATTENTION_Q, "kernel")])
+                .view(self.hidden_size, self.hidden_size)
+                .t()
+            )
+            key_weight = (
+                np2th(weights[pjoin(ROOT, ATTENTION_K, "kernel")])
+                .view(self.hidden_size, self.hidden_size)
+                .t()
+            )
+            value_weight = (
+                np2th(weights[pjoin(ROOT, ATTENTION_V, "kernel")])
+                .view(self.hidden_size, self.hidden_size)
+                .t()
+            )
+            out_weight = (
+                np2th(weights[pjoin(ROOT, ATTENTION_OUT, "kernel")])
+                .view(self.hidden_size, self.hidden_size)
+                .t()
+            )
 
             query_bias = np2th(weights[pjoin(ROOT, ATTENTION_Q, "bias")]).view(-1)
             key_bias = np2th(weights[pjoin(ROOT, ATTENTION_K, "bias")]).view(-1)
@@ -293,22 +311,22 @@ class Transformer(nn.Module):
 class VisionTransformer(nn.Module):
     def __init__(self, config, img_size=224, vis=False):
         super(VisionTransformer, self).__init__()
-        #self.num_classes = num_classes
-        #self.classifier = config.classifier
+        # self.num_classes = num_classes
+        # self.classifier = config.classifier
         self.embed_dim = config.hidden_size
 
         self.transformer = Transformer(config, img_size, vis)
-        #self.head = Linear(config.hidden_size, num_classes)
+        # self.head = Linear(config.hidden_size, num_classes)
 
     def forward(self, x, labels=None, use_patches=False):
         x, attn_weights = self.transformer(x)
-        #logits = self.head(x[:, 0])
+        # logits = self.head(x[:, 0])
 
-        #if labels is not None:
+        # if labels is not None:
         #    loss_fct = CrossEntropyLoss()
         #    loss = loss_fct(logits.view(-1, self.num_classes), labels.view(-1))
         #    return loss
-        #else:
+        # else:
         #    return logits, attn_weights
 
         if use_patches:
@@ -318,25 +336,36 @@ class VisionTransformer(nn.Module):
 
     def load_from(self, weights):
         with torch.no_grad():
-            #if self.zero_head:
+            # if self.zero_head:
             #    nn.init.zeros_(self.head.weight)
             #    nn.init.zeros_(self.head.bias)
-            #else:
+            # else:
             #    self.head.weight.copy_(np2th(weights["head/kernel"]).t())
             #    self.head.bias.copy_(np2th(weights["head/bias"]).t())
 
-            self.transformer.embeddings.patch_embeddings.weight.copy_(np2th(weights["embedding/kernel"], conv=True))
-            self.transformer.embeddings.patch_embeddings.bias.copy_(np2th(weights["embedding/bias"]))
+            self.transformer.embeddings.patch_embeddings.weight.copy_(
+                np2th(weights["embedding/kernel"], conv=True)
+            )
+            self.transformer.embeddings.patch_embeddings.bias.copy_(
+                np2th(weights["embedding/bias"])
+            )
             self.transformer.embeddings.cls_token.copy_(np2th(weights["cls"]))
-            self.transformer.encoder.encoder_norm.weight.copy_(np2th(weights["Transformer/encoder_norm/scale"]))
-            self.transformer.encoder.encoder_norm.bias.copy_(np2th(weights["Transformer/encoder_norm/bias"]))
+            self.transformer.encoder.encoder_norm.weight.copy_(
+                np2th(weights["Transformer/encoder_norm/scale"])
+            )
+            self.transformer.encoder.encoder_norm.bias.copy_(
+                np2th(weights["Transformer/encoder_norm/bias"])
+            )
 
             posemb = np2th(weights["Transformer/posembed_input/pos_embedding"])
             posemb_new = self.transformer.embeddings.position_embeddings
             if posemb.size() == posemb_new.size():
                 self.transformer.embeddings.position_embeddings.copy_(posemb)
             else:
-                print("load_pretrained: resized variant: %s to %s" % (posemb.size(), posemb_new.size()))
+                print(
+                    "load_pretrained: resized variant: %s to %s"
+                    % (posemb.size(), posemb_new.size())
+                )
                 ntok_new = posemb_new.size(1)
 
                 if self.classifier == "token":
@@ -347,7 +376,7 @@ class VisionTransformer(nn.Module):
 
                 gs_old = int(np.sqrt(len(posemb_grid)))
                 gs_new = int(np.sqrt(ntok_new))
-                print('load_pretrained: grid-size from %s to %s' % (gs_old, gs_new))
+                print("load_pretrained: grid-size from %s to %s" % (gs_old, gs_new))
                 posemb_grid = posemb_grid.reshape(gs_old, gs_old, -1)
 
                 zoom = (gs_new / gs_old, gs_new / gs_old, 1)
@@ -361,7 +390,9 @@ class VisionTransformer(nn.Module):
                     unit.load_from(weights, n_block=uname)
 
             if self.transformer.embeddings.hybrid:
-                self.transformer.embeddings.hybrid_model.root.conv.weight.copy_(np2th(weights["conv_root/kernel"], conv=True))
+                self.transformer.embeddings.hybrid_model.root.conv.weight.copy_(
+                    np2th(weights["conv_root/kernel"], conv=True)
+                )
                 gn_weight = np2th(weights["gn_root/scale"]).view(-1)
                 gn_bias = np2th(weights["gn_root/bias"]).view(-1)
                 self.transformer.embeddings.hybrid_model.root.gn.weight.copy_(gn_weight)
