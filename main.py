be8792e8706245a8061678d9e67699543aad75d2
Fabio Fehr <fabio.fehr@idiap.ch>
2024-04-20 18:21:35 +0200

    silly comment broke things

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ def main(args):
 
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
-    # max_accuracy = test_stats["acc1"]
+    max_accuracy = test_stats["acc1"]
 
     for epoch in range(args.start_epoch, args.epochs):
 
